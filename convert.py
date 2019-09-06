beef354107f3603dfb8b36facd2f8057a26c0319
Dave Dowding <dave.dowding@geosmartdecisions.co.uk>
2019-09-06 21:42:08 +0100

    now converts all files into seperate markdown files

--- convert.py
+++ convert.py
@@ -1,15 +1,24 @@
+import os
 import json
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
+#file = open("output.md","w")
+directory='../books/'
+for filename in os.listdir(directory):
+    if filename.endswith(".json"):
+        with open(os.path.join(directory, filename)) as json_file:
+            data = json.load(json_file)
 
-with open('highlights.json') as json_file:
-    data = json.load(json_file)
+            file=open((directory+filename+".md"),"w")
 
-file = open("output.md","w")
-file.write("## "+data['title']+"\n\nAuthor: "+data['authors']+"\n\n")
+            file.write("## "+data['title']+"\n\nAuthor: "+data['authors']+"\n\n")
 
-for h in data['highlights']:
-    file.write ("> "+h['text']+" (["+str(h['location']['value'])+"]("+h['location']['url']+")) \n\n")
+            for h in data['highlights']:
+                file.write ("> "+h['text']+" (["+str(h['location']['value'])+"]("+h['location']['url']+")) \n\n")
 
-file.close()
+
+        continue
+    else:
+        continue
+#file.close()
